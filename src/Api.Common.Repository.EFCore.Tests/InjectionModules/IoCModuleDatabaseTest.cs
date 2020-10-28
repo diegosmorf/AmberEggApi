a8d2481f2cf5d6c2fa7d1206d347165d712d3a15
Diego Cardoso <DOCS@gft.com>
2020-10-28 09:57:03 -0300

    add tests for repository efcore

---
+++ src/Api.Common.Repository.EFCore.Tests/InjectionModules/IoCModuleDatabaseTest.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using System.Reflection;
+using Module = Autofac.Module;
+
+namespace Api.Common.Repository.EFCore.Tests.InjectionModules
+{
+    public class IoCModuleDatabaseTest : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var assemblyToScan = Assembly.GetAssembly(typeof(IoCModuleDatabaseTest));
+
+            builder
+                .RegisterAssemblyTypes(assemblyToScan)
+                .Where(c => c.IsClass
+                            && c.IsInNamespace("Api.Common.Repository.EFCore.Tests.Factories")).AsSelf();
+                        
+        }
+    }
+}
---
+++ src/Api.Common.Repository.EFCore.Tests/UnitTests/SetupTests.cs
@@ -0,0 +1,42 @@
+﻿using AmberEggApi.ApplicationService.InjectionModules;
+using AmberEggApi.Database.Repositories;
+using AmberEggApi.Domain.Tests.InjectionModules;
+using AmberEggApi.Infrastructure.InjectionModules;
+using Api.Common.Repository.EFCore.Tests.InjectionModules;
+using Autofac;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace Api.Common.Repository.EFCore.Tests.UnitTests
+{
+    [SetUpFixture]
+    public class SetupTests
+    {
+        public static IContainer Container { get; protected set; }
+
+        [OneTimeSetUp]
+        public void RunBeforeAllTests()
+        {
+            // Setup IoC Container
+            var builder = new ContainerBuilder();
+            builder.RegisterModule(new IoCModuleApplicationService());
+            builder.RegisterModule(new IoCModuleInfrastructure());
+            builder.RegisterModule(new IoCModuleAutoMapper());
+            builder.RegisterModule(new IoCModuleDomainTest());
+            builder.RegisterModule(new IoCModuleDatabaseTest());
+
+            var opt = new DbContextOptionsBuilder<EfCoreDbContext>();
+            opt.UseInMemoryDatabase(databaseName: "AmberEgg-API-EFCore-Db");
+
+            builder.RegisterInstance(new EfCoreDbContext(opt.Options)).As<DbContext>();
+
+            Container = builder.Build();
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAllTests()
+        {
+            Container.Dispose();
+        }
+    }
+}
